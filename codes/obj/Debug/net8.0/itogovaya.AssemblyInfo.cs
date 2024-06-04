21818f56bedb1ad72f0619a8aec007cfa7df4569
supermightybeast <imbahate@gmail.com>
2024-06-04 12:54:16 +0500

    9 PUSH

--- codes/obj/Debug/net8.0/itogovaya.AssemblyInfo.cs
+++ codes/obj/Debug/net8.0/itogovaya.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("itogovaya")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b834222952d3c56ba98b0fa42da80a49bb48291f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69177689a7a35186df5cb53eb7fe502331d2cc96")]
 [assembly: System.Reflection.AssemblyProductAttribute("itogovaya")]
 [assembly: System.Reflection.AssemblyTitleAttribute("itogovaya")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
